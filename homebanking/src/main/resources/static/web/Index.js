bc9df64e48ba63e6dc63253a4b9dc1d84b2fc1b8
NoFee93 <francobrizzio@gmail.com>
2023-01-30 16:59:31 -0300

    index error

--- homebanking/src/main/resources/static/web/Index.js
+++ homebanking/src/main/resources/static/web/Index.js
@@ -29,11 +29,28 @@ const app = createApp({
             this.validated = 1
             if (this.userInfo.includes("@admin.com"))
             {
-              axios.post('/api/login',`email=${this.userInfo}&password=${this.password}`).then(response => console.log(response))
-              setTimeout(()=>{ window.location = ("/h2-console");}, 1000);
+              axios.post('/api/login',`email=${this.userInfo}&password=${this.password}`)
+              .then((response) => 
+              {console.log(response)
+              setTimeout(()=>{ window.location = ("/h2-console");}, 1000);})
+              .catch(function (error) {
+                if (error.response) {
+                  alert(error.response.data);
+                  location.reload()
+                  console.log(error.response.data);
+                  console.log(error.response.status);
+                  console.log(error.response.headers);
+                } else if (error.request) {
+                  console.log(error.request);
+                } else {
+                  console.log('Error', error.message);
+                }
+                console.log("Hola");
+              }); 
             }else{
-              axios.post('/api/login',`email=${this.userInfo}&password=${this.password}`).then(response => console.log(response))
-                setTimeout(()=>{ window.location = ("/web/accounts.html");}, 2000);   
+              axios.post('/api/login',`email=${this.userInfo}&password=${this.password}`)
+              .then(response => console.log(response))
+                setTimeout(()=>{ window.location = ("/web/accounts.html");}, 2000);  
             }
           },
           getpass(){
